16d8eac5cd89133debdc892cfa48775be86068b1
JordanBell98 <bel21035@byui.edu>
2022-10-29 21:10:42 -0600

    adding discovery page

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -6,3 +6,17 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+const visitDate = localStorage.setItem("visitDate", visitDate);
+const todaysDate = Date.now();
+localStorage.setItem("visitDate", todaysDate);
+const comparedDate = todaysDate - visitDate;
+
+function days() {
+    if (visitDate = null) {
+        document.querySelector("#lastVisit").innerText = "This is your first visit";
+    } else {
+        document.querySelector("lastVisit").innerText = comparedDate;
+    }
+}
+
